89a4c59e0f28a819b05c5bb3d8fb84e72feb13c4
Mega-117 <enricosaian@hotmail.it>
2021-11-22 20:13:55 +0100

    fatto sezione promo

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,10 +1,30 @@
 <template>
-  <div></div>
+  <main>
+    <Hero></Hero>
+    <div class="promo">
+      <div class="container">
+        <div class="d-flex justify-content-center align-items-center">
+          <img src="../assets/food-transparent-5-200x112.png" alt="" />
+          <span>
+            Get $25 OFF your first purchase of our homemade pet food!</span
+          >
+          <a href="#">Visit the shop</a>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
-export default {};
+import Hero from "@/components/Hero.vue";
+export default {
+  name: "Main",
+  components: {
+    Hero,
+  },
+};
 </script>
 
-<style>
+<style scoped lang="scss">
+@import "@/styles/main.scss";
 </style>
