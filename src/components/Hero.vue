23bbd209ef56165461e407667278ba69549db3b0
Mega-117 <enricosaian@hotmail.it>
2021-11-22 20:13:09 +0100

    fatto hero

---
+++ src/components/Hero.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hero">
+    <div class="hero-overlay">
+      <div class="container">
+        <div class="hero-text-container">
+          <small>find the best anima supplies</small>
+          <h1>
+            We know animals are a part of yiur family, let us help take care of
+            them.
+          </h1>
+
+          <button>Learn more about us</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@import "@/styles/hero.scss";
+</style>
